8487bba32ff537ab03a1977b13661b29e82b2009
Garv Bahl <bhlgarv@gmail.com>
2026-07-14 12:39:13 -0700

    Remove Midmark investigation scratch files from repo

--- vetce/find_online_link.py
+++
@@ -1,7 +0,0 @@
-import re
-html = open("midmark.html", encoding="utf-8").read()
-# Find links near "online" / "View Online"
-for m in re.finditer(r'(View Online[^<]*|dentistry-online|Online Course[^<]*)', html, re.IGNORECASE):
-    start = max(0, m.start()-300)
-    print("---")
-    print(html[start:m.end()+100])
